6b6d43fe9a40948a8f354782a2086ca59ba5c3fa
MehaiIulian <61966483+MehaiIulian@users.noreply.github.com>
2024-02-05 21:45:23 +0200

    Update

--- SORE/adjust_preferences.py
+++ SORE/adjust_preferences.py
@@ -8,6 +8,45 @@ from rdflib import Graph
 import requests
 
 
+
+
+def get_medical_news_for_email(g, email):
+    sparql_query_medical = f"""
+        PREFIX ns1: <http://visualdataweb.org/SoreOntology/>
+        PREFIX ns2: <http://visualdataweb.org/SoreOntology/personOntology/>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        
+        SELECT DISTINCT ?medicalNews ?title ?author ?description ?hasMedicalUrl
+        WHERE {{
+          ?user ns2:hasEmail "{email}" .
+          ?user ns2:hasRecommendedItem ?medicalNews .
+          ?medicalNews a ns1:Medical_and_Lifestyle .
+          ?medicalNews ns1:hasTitle ?title .
+          OPTIONAL {{ ?medicalNews ns1:hasAuthor ?author . }}
+          OPTIONAL {{ ?medicalNews ns1:hasDescription ?description . }}
+          OPTIONAL {{ ?medicalNews ns1:hasMedicalUrl ?url . }}
+        }}
+        """
+    print('here')
+
+    results = g.query(sparql_query_medical)
+
+    medical_data = []
+    for result in results:
+        medical_entry = {
+            'medicalNews': str(result['medicalNews']),
+            'title': str(result['title']),
+            'author': str(result['author']) if result['author'] else None,
+            'description': str(result['description']) if result['description'] else None,
+            'hasMedicalUrl': str(result['hasMedicalUrl']) if result['hasMedicalUrl'] else None,
+        }
+        print(medical_entry)
+        medical_data.append(medical_entry)
+
+    return medical_data
+
+
 def get_news_for_email(g, email):
     sparql_query = f"""
     PREFIX ns1: <http://visualdataweb.org/SoreOntology/>
@@ -47,8 +86,7 @@ def get_news_for_email(g, email):
 
     return news_data
 
-def get_medical_news_for_email(g,email):
-    pass
+
 
 def get_most_words_news(users_graph_directory, email):
     g = Graph()
@@ -67,8 +105,15 @@ def get_most_words_news(users_graph_directory, email):
 
     filtered_tokens = ' '.join(word for word in tokens if word not in stopwords)
 
-    words = get_most_relevant_words(filtered_tokens)
-    return words
+    words_news = get_most_relevant_words(filtered_tokens)
+
+    text = get_medical_news_for_email(g, email)
+    print(text)
+
+    words_medical_news = ['test']
+
+
+    return words_news, words_medical_news
 
 
 def get_most_relevant_words(article_text):
@@ -95,13 +140,20 @@ def get_most_relevant_words(article_text):
 
 
 #Testing
-users_graph_directory = "./"
-email = "john_smith@example.com"
-new_news_interest = get_most_words_news(users_graph_directory,email)
-print(new_news_interest)
-email = "ab@gmail.com"
-users_graph_directory = './users_graph/'
-new_news_interest1 = get_most_words_news(users_graph_directory,email)
-print(new_news_interest1)
+users_graph_directory = "./users_graph/"
+email = "stoleriumihai@gmail.com"
+new_news_interest,new_medical_news_interest = get_most_words_news(users_graph_directory,email)
 
-# def update_user_graph(email, new_news_interest):
+print(new_news_interest,new_medical_news_interest)
+
+email = "tudor.tomescu@gmail.com"
+users_graph_directory = './users_graph/'
+new_news_interest1,new_medical_news_interest1 = get_most_words_news(users_graph_directory,email)
+
+print(new_news_interest1,new_medical_news_interest1)
+
+
+
+
+
+
